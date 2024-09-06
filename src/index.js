9fecf5ae46aa90bafc7d32240fef20931e089f2c
Vishalsahani156 <vishalsahani1444@gmail.com>
2024-09-06 18:28:35 +0530

    utils code discussion

--- src/index.js
+++ src/index.js
@@ -1,2 +1,17 @@
 import connectDB  from "./DBconnected/indexDB.js";
+
 connectDB()
+.then(()=>{
+    application.liste(process.env.PORT||8000,()=>{
+        console.log(`Server is running  at port${process.env.PORT}`);
+
+    })
+    app.on("error",(E)=>{
+        console.log("error",E);
+        throw E
+    })
+})
+.catch((e)=>{
+    console.log(" MongoDB data base  connection fail ",e);
+
+})
